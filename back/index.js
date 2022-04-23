2c2ade76c67624f4e865e308040fe9da983c906e
tolgu <jayjay.gee1020@gmail.com>
2022-04-24 01:07:13 +0900

    Sequelize 사용하여 AWS-MySQL 연동 성공

--- back/index.js
+++ back/index.js
@@ -1,13 +1,29 @@
-import "dotenv/config";
-import express from "express";
-
+const mysql = require('mysql');
+const express = require('express');
 const app = express();
-const PORT = process.env.SERVER_PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`정상적으로 서버를 시작하였습니다. http://localhost:${PORT}`);
+const conn = {
+  host: "tolgu.ctejro5fgaj9.ap-northeast-2.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "Aa!23456",
+  database: "team17",
+}
+
+app.get('/', function (req, res) {
+
+  let connection = mysql.createConnection(conn);
+  connection.connect();
+
+  const testQuery = "SELECT * FROM Drawer";
+
+  connection.query(testQuery, (error, result, field) => {
+    if (error) {
+      console.log("ERrror Execution :", error);
+    }
+    res.send(result);
+  });
+  connection.end();
 });
 
-app.get("/", (req, res) => {
-  res.send("성공입니다.");
-});
+app.listen(3000, () => console.log('start...'));
