209b63b169990abecaf01c5e267f0739d30be87c
GeonTae Park <rssr96@naver.com>
2022-04-28 03:50:12 +0900

    [ADD] 로컬에서 user-mvp 머지

--- back/index.js
+++ back/index.js
@@ -1,25 +1,43 @@
 import cors from "cors";
 import modelManager from "./db/models";
-import { sysLog, sysErrorLog, DatabaseError } from "./src/utils";
+import { sysLog, sysErrorLog, DatabaseError } from "./src/utils/logger";
 import mysqlManager from "./db";
 import * as dotenv from "dotenv";
+import swaggerDocument from "./src/swagger.json";
 import express from "express";
-import postingRouter from "./src/routers/postingRouter";
+import { swaggerUi, specs } from "./src/modules/swagger";
+import userAuthRouter from "./src/routers/userRouter";
+import passport from "passport";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import passportConfig from "./passport";
 
 dotenv.config();
+
+passportConfig();
 
 const swaggerDocument = require("./src/swagger.json");
 const { swaggerUi, specs } = require("./src/modules/swagger");
 const app = express();
-const PORT = 5002;
 
-dotenv.config();
-console.log(process.env.DBUSERNAME);
+const PORT = 5001;
+
 mysqlManager.connect();
 
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.PASSWORD,
+  }),
+);
+
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", function (req, res) {
   res.send("Hello Express");
@@ -27,13 +45,9 @@ app.get("/", function (req, res) {
 
 app.use(postingRouter);
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
+app.use(userAuthRouter);
 
-// swagger 확인용
-app.post("/check", (req, res) => {
-  console.log(req.body);
-  res.json({ isStatus: 200 });
-});
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
 
 app.listen(PORT, () => {
   console.log(`정상적으로 서버를 시작하였습니다. http://localhost:${PORT}`);
--- back/src/modules/swagger.js
+++ back/src/modules/swagger.js
@@ -2,23 +2,22 @@ import swaggerUi from "swagger-ui-express";
 import swaggerJsDoc from "swagger-jsdoc";
 
 const options = {
-    swaggerDefinition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Express Service with Swagger!",
-        version: "1.0.0",
-        description: "A REST API using swagger and express.",
-      },
-      servers: [
-        {
-          url: "http://localhost:5001",
-        },
-        
-      ],
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Express Service with Swagger!",
+      version: "1.0.0",
+      description: "A REST API using swagger and express.",
     },
-    apis: [],
-  };
+    servers: [
+      {
+        url: "http://localhost:5001",
+      },
+    ],
+  },
+  apis: [],
+};
 
 const specs = swaggerJsDoc(options);
 
-export {swaggerUi, specs};
+export { swaggerUi, specs };
