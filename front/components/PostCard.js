7f15d5b7e4ce5849b2fa1794939dfab335772e9d
leewol <lsee5265@gmail.com>
2022-04-28 16:29:34 +0900

    [ADD] Posting MVP 구현 파일

---
+++ front/components/PostCard.js
@@ -0,0 +1,18 @@
+import { Card } from "@mui/material";
+
+// TODO : 피드 레이아웃 완성
+// * 사진, 내용, 좋아요, 댓글, 댓글 달기
+
+export default function PostCard() {
+  return (
+    <div>
+      {/* <Card>
+        <Image />
+        <Content />
+        <Buttons></Buttons>
+      </Card>
+      <CommentForm />
+      <Comment /> */}
+    </div>
+  );
+}
---
+++ front/pages/posting.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+
+// TODO : 게시글 피드 형태로 보여 주기
+// * 상태 : 작성 가능한지, 편집 중인지
+// * 모든 게시글 데이터 받아오기 (등록 시간 최근 순)
+export default function Posting() {
+  return <div></div>;
+}
