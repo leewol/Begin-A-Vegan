4b219a4dfc7a76bcbf777e1fcdef73dde6654d12
leewol <lsee5265@gmail.com>
2022-05-04 11:42:35 +0900

    [ADD] Quill 게시글 작성
    - 특이사항: react,react-dom 버전 18.0.0 -> 17.0.2 다운그레이드 적용

--- front/components/PostCreator.js
+++ front/components/PostCreator.js
@@ -1,4 +1,8 @@
+import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
+
+import styled from "styled-components";
+import Button from "@mui/material/Button";
 
 /* 
   * document 조작의 순서 : 
@@ -7,33 +11,48 @@ import dynamic from "next/dynamic";
     => document is not defined 오류
 */
 
-const QuillWrapper = dynamic(
-  () =>
-    import("react-quill").then((mod) => {
-      // 이 설정 안 해주면 선택 바가 두 개 보임 (React18의 문제인 듯)
-      // ? 부모인 ReactQuill을 상속받은 QuillWrapper 클래스에서 내부적으로 뭔가 수정?
-      class QuillWrapper extends mod.default {
-        destroyEditor() {
-          if (!this.editor) return;
-          this.unhookEditor(this.editor);
-        }
-      }
-      return QuillWrapper;
-    }),
-  {
-    ssr: false,
-    loading: () => <p>Loading...</p>,
-  },
-);
+const QuillWrapper = dynamic(() => import("react-quill"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
 
 export default function PostCreator() {
+  const [description, setDescription] = useState();
+  const [postable, setPostable] = useState(false);
+
+  // const validatePost = () => {
+  //   if (description.length == 0)
+  // }
+  const handleQuillSubmit = () => {};
+
+  const formats = ["bold", "italic", "underline", "strike", "image", "clean"];
   const modules = {
     toolbar: [["bold", "italic", "underline", "strike"], ["image"], ["clean"]],
     clipboard: {
       // toggle to add extra line breaks when pasting HTML:
       matchVisual: false,
     },
+    handler: {
+      image: imageHandler,
+    },
   };
 
-  return <QuillWrapper theme="snow" modules={modules} placeholder={"포스팅 내용을 입력하세요"} />;
+  return (
+    <form onSubmit={handleQuillSubmit}>
+      <QuillWrapper
+        theme="snow"
+        modules={modules}
+        formats={formats}
+        value={description}
+        onChange={(value, delta, source, editor) =>
+          console.log(editor.getLength(), editor.getText())
+        }
+        placeholder={"포스팅 내용을 입력하세요"}
+      />
+      {/* 사진 & 글 내용 없으면 disabled */}
+      <Button type="submit" variant="contained" size="small">
+        공유하기
+      </Button>
+    </form>
+  );
 }
