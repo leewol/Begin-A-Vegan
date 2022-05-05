2fed064bd8fe2d46bb5a5b59718b6717ff87d3e5
Suyoun <sykim51774@gmail.com>
2022-05-05 21:25:02 +0900

    [ADD] 로그인 페이지 수정

---
+++ front/pages/login.js
@@ -0,0 +1,70 @@
+import React, { useState, useContext, createContext } from "react";
+import Header from "../components/Header";
+
+import * as Api from "../lib/api";
+
+export default function Login() {
+  const DispatchContext = createContext(null);
+
+  const dispatch = useContext(DispatchContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const res = await Api.post("/login", {
+        email,
+        password,
+      });
+      const user = res.data;
+      const token = user.token;
+
+      sessionStorage.setItem("userToken", token);
+
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: user,
+      });
+    } catch (error) {
+      console.log("로그인에 실패하였습니다.\n", error);
+    }
+  };
+
+  return (
+    <div>
+      <Header />
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="EMAIL"
+            value={email}
+            onChange={onEmailHandler}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="PASSWORD"
+            value={password}
+            onChange={onPasswordHandler}
+          />
+        </div>
+        <button type="submit">LOGIN</button>
+      </form>
+    </div>
+  );
+}
