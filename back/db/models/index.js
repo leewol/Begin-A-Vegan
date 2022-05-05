4be18ac8c4013e62eeec1febe3ab5b0952172710
Kimwater <iamkimwater@gmail.com>
2022-05-05 23:24:09 +0900

    [FIX] associate models

--- back/db/models/index.js
+++ back/db/models/index.js
@@ -3,15 +3,19 @@ import Posting from "./posting";
 import User from "./user";
 
 class ModelManager {
-  initialize(sequelize) {
+  async initialize(sequelize) {
     // 각각 모델의 dependency가 있기 때문에 순서에 맞게 sync를 해야 합니다.
     User.init(sequelize);
     Posting.init(sequelize);
     Comment.init(sequelize);
 
-    User.sync();
-    Posting.sync();
-    Comment.sync();
+		await User.sync();
+    await Posting.sync();
+    await Comment.sync();
+
+    User.associate(sequelize.models);
+    Posting.associate(sequelize.models);
+    Comment.associate(sequelize.models);
   }
 }
 
