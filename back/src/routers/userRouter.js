b2514300e546c0b6759444811454fb20b42417de
Kimwater <iamkimwater@gmail.com>
2022-05-07 23:42:46 +0900

    [FIX] user profile url field typo

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -114,7 +114,7 @@ userAuthRouter.patch("/users/:id", async (req, res, next) => {
 // storage 옵션만 s3로 바꾸면 멀터가 알아서 스토리지로 올려줌
 userAuthRouter.post("/profile", upload.single("image"), login_required, async (req, res) => {
   console.log(req.file);
-  await req.user.update({ profile_url: req.file.path });
+  await req.user.update({ profile_url: req.file.location });
   res.json(req.file);
 });
 //upload array = 여러장 / single = 한장
